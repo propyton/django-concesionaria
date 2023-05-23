f63478f9a55abf96f8f7a14df2e20b6cd5c5cbef
Diego Ignacio Figueroa Varas <diegofigueroavaras@gmail.com>
2023-05-22 21:49:02 -0400

    views

--- menu/views.py
+++ menu/views.py
@@ -7,3 +7,39 @@ def CambiarContrasenia(request):
 
 def Camionetas(request):
     return render(request, 'menu/Camionetas.html')
+
+def FordF150Platinum(request):
+    return render(request, 'menu/Camionetas.html')
+
+def FordF150Raptor(request):
+    return render(request, 'menu/Camionetas.html')
+
+def InicioDesesion(request):
+    return render(request, 'menu/Camionetas.html')
+
+def jeep(request):
+    return render(request, 'menu/Camionetas.html')
+
+def Mazda(request):
+    return render(request, 'menu/Camionetas.html')
+
+def MenuPrincipal(request):
+    return render(request, 'menu/Camionetas.html')
+
+def PerfilDeUsuario(request):
+    return render(request, 'menu/Camionetas.html')
+
+def portada(request):
+    return render(request, 'menu/Camionetas.html')
+
+def RegistroDeUsuario(request):
+    return render(request, 'menu/Camionetas.html')
+
+def Silverado(request):
+    return render(request, 'menu/Camionetas.html')
+
+def subaru(request):
+    return render(request, 'menu/Camionetas.html')
+
+def Suv(request):
+    return render(request, 'menu/Camionetas.html')
